0e0b0a86d72a7dc627a369a67da8a475eaf3f068
Vladimir <mrdrouf777@yandex.ru>
2023-11-30 15:33:58 +0300

    beautify rest methods calling

--- ProvidersServiceOrders/Classes/Extensions/RestMethodHelper.cs
+++ ProvidersServiceOrders/Classes/Extensions/RestMethodHelper.cs
@@ -1,5 +1,10 @@
 ﻿using ProvidersServiceOrders.Classes.Rest;
+using ProvidersServiceOrders.Interfaces;
+using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace ProvidersServiceOrders.Classes.Extensions
 {
@@ -9,6 +14,14 @@ namespace ProvidersServiceOrders.Classes.Extensions
         public static RestMethod Configure(this MethodBase method, object inputValue = null)
         {
             return new RestMethod(method).Configure(inputValue);
+        }
+        public static MethodBase GetCurrentMethod(this IRest rest, [CallerMemberName] string memberName = "")
+        {
+            return MethodBase.GetMethodFromHandle(rest.GetType().GetMethods().First(m => m.Name == memberName).MethodHandle);
+        }
+        public static ReturnType PerformTypeConversion<ReturnType>(this object obj)
+        {
+            return (ReturnType)obj;
         }
     }
 }
